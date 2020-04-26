b99ce45b84dcf4fef7d05c753346745bed968817
Rafael Costa Palma <rafaelcostapalma@gmail.com>
2020-04-26 17:07:43 -0300

    adding tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,41 @@
-import styled from 'styled-components';
+/* eslint-disable import/prefer-default-export */
+import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
-  color: #666360;
-
   display: flex;
   align-items: center;
+
+  border: 2px solid #232129;
+  color: #666360;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) => props.isErrored
+    && css`
+    border-color: #c53030;`}
+
+  ${(props) => props.isFocused
+    && css`
+    border-color: #ff9000;
+    color: #ff9000;`}
+
+  ${(props) => props.isFilled
+    && css`
+    color: #ff9000;`}
+
 
   input {
     flex: 1;
@@ -24,11 +46,27 @@ export const Container = styled.div`
     &::placeholder{
       color: #666360;
     }
-
-
   }
 
   svg {
     margin-right: 16px;
   }
 `;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg{
+    margin: 0px;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+
+  }`;
